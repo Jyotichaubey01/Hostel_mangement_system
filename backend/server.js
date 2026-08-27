9ac7750e8ab83cb97abd371a42a412f5523c9bbc
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-08-27 23:38:40 +0530

    commit00

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/authRoutes'));
+
+app.get('/', (req, res) => {
+  res.send('Hostel Management System API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
